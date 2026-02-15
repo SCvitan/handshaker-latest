4452504760a405ccadcbb5bea86f952f9a2e37f4
SCvitan <sime.cvitan25@gmail.com>
2026-02-16 00:20:44 +0100

    added CORS

--- handshaker-backend/auth-service/src/main/java/com/handshaker/auth_service/config/CorsConfig.java
+++ handshaker-backend/auth-service/src/main/java/com/handshaker/auth_service/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://142.132.181.45:3000/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
